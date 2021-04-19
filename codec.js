39c14fb884b749f1535eeeabbede0b568c999d1a
rodispantelis <32998445+rodispantelis@users.noreply.github.com>
2021-04-20 00:16:24 +0300

    Add files via upload

--- codec.js
+++ codec.js
@@ -13,12 +13,32 @@
 	Published by Pantelis Rodis, 2021
 */	
 
-function coder(){
+function coder(a, b){
+var res=0;
+//on input of two nodes, you get the place of their edge on the vector
+var k=0;
+if((b-a)===0){
+}else{
+    if((b-a)<0){
+        k=((a-1)*(1+(a-1)))/2;
+        res=Number(k)+Number(b);
+    }else{
+        k=((b-1)*(1+(b-1)))/2;
+        res=Number(k)+Number(a);
+    }
+}
+a=0;
+b=0;
+return res;
+}
+
+function coder1(){
 var a0=document.getElementById("coda").value;
 var b0=document.getElementById("codb").value;
 document.getElementById("resultcod").innerHTML="";
 coder2(a0, b0);
 }
+
 function coder2(a, b){
 var res=0;
 //on input of two nodes, you get the place of their edge on the vector
@@ -43,11 +63,36 @@ a=0;
 b=0;
 }
 
-function decoder(){
+function decoder(q){
+//on input of one place of the vector that denotes one edge of the graph
+//you get the two nodes that define the edge
+//x<y
+var x=0;
+var y=0;
+var res;
+
+y=Math.floor(Math.sqrt((2*q)));
+//y=Math.sqrt((2*q));
+y++;
+x=q-(((y*y)-y)/2);
+if(x<0){
+	y--;
+	x=q-(((y*y)-y)/2);
+}		
+
+res=[x,y];
+x=0;
+y=0;
+q=0;
+return res;
+}
+
+function decoder1(){
 var q0=document.getElementById("decod").value;
 document.getElementById("resultdecod").innerHTML="";
 decoder2(q0);
 }
+
 function decoder2(q){
 //on input of one place of the vector that denotes one edge of the graph
 //you get the two nodes that define the edge
